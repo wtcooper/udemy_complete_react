3845fceb4ea579a085227ab3280ab64d1ea032ff
Wade Cooper <wade.t.cooper@pwc.com>
2022-03-06 17:56:29 -0500

    adding remove item from cart (end 8.149) end lesson-20

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider} from "react-redux";
+import { Provider } from 'react-redux';
 
 import store from './redux/store';
 
@@ -9,11 +9,10 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>,
-
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
