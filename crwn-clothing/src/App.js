b68f34ec5b28b27baab08d07087982e286270251
Wade Cooper <wade.t.cooper@pwc.com>
2022-03-06 15:38:20 -0500

    initial redux setup (section 8.128), end lesson-12

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -8,36 +9,49 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
+
 
 class App extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            currentUser: null
-        };
-    }
+    // pre-redux - this is setting state directly
+    // constructor() {
+    //     super();
+    //
+    //     this.state = {
+    //         currentUser: null
+    //     };
+    // }
 
     unsubscribeFromAuth = null;
 
     componentDidMount() {
+        const { setCurrentUser } = this.props;
+
+
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
             if (userAuth) {
                 const userRef = await createUserProfileDocument(userAuth);
 
                 userRef.onSnapshot(snapShot => {
-                    this.setState({
-                        currentUser: {
-                            id: snapShot.id,
-                            ...snapShot.data()
-                        }
-                    });
+                    // pre-redux
+                    // this.setState({
+                    //     currentUser: {
+                    //         id: snapShot.id,
+                    //         ...snapShot.data()
+                    //     }
+                    // });
+                    // console.log(this.state); //logs the current user, pre-redux
 
-                    // console.log(this.state); logs the current user
+                    setCurrentUser({
+                        id: snapShot.id,
+                        ...snapShot.data()
+                    });
                 });
             }
 
-            this.setState({ currentUser: userAuth });
+            // pre-redux
+            // this.setState({ currentUser: userAuth });
+            setCurrentUser(userAuth);
         });
     }
 
@@ -48,7 +62,10 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Header currentUser={this.state.currentUser} />
+                {/*this is pre-redux - using state directly*/}
+                {/*<Header currentUser={this.state.currentUser} />*/}
+                <Header  />
+
                 <Switch>
                     <Route exact path='/' component={HomePage} />
                     <Route path='/shop' component={ShopPage} />
@@ -59,4 +76,11 @@ class App extends React.Component {
     }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+    setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(App);
